a5c79dc0a30c0bf861340c7808cab608d1f472e5
Sebastian Raimondi <sebastianraimondi@gmail.com>
2018-11-18 15:01:07 -0300

    Modified apis, removed last /

--- apis.js
+++ apis.js
@@ -1,7 +1,7 @@
 var apis = {
-    stock: 'http://localhost:3000/api/',
-    staff: 'http://localhost:3001/api/',
-    sales: 'http://localhost:3002/api/',
+    stock: 'http://localhost:3000/api',
+    staff: 'http://localhost:3001/api',
+    sales: 'http://localhost:3002/api',
 }
 
 module.exports = apis;
