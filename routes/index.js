24ee3c65c349d550734c33d63c7b23edb7cd4199
Sebastian Raimondi <sebastianraimondi@gmail.com>
2018-11-18 14:44:51 -0300

    Add /products

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const fetch = require("node-fetch");
+
+const apis = require('../apis.js');
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Backend' });
+});
+
+router.get('/products', async function(req, res, next) {
+  let response = await fetch(apis.stock + 'products');
+  res.json(await response.json());
 });
 
 module.exports = router;
